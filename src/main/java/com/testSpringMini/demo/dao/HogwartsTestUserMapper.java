0352966e2dcabaf1296726298dc6bb7444e2d32a
jingxueru <jingxueru@tezign.com>
2021-04-08 12:25:30 +0800

    - 数据持久化 - mybatis-maven依赖 - 自动生成代码-maven配置 - mybatis配置 - 自动生成代码- config.properties - 自动生成代码- generatorConfig.xml - 用户表 - tk.mybatis使用- mapper 统一父类

--- src/main/java/com/testSpringMini/demo/dao/HogwartsTestUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.testSpringMini.demo.dao;
-
-import com.testSpringMini.demo.common.MySqlExtensionMapper;
-import com.testSpringMini.demo.entity.HogwartsTestUser;
-
-public interface HogwartsTestUserMapper extends MySqlExtensionMapper<HogwartsTestUser> {
-}
