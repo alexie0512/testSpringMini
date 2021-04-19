cd3ddaba6a2cf2c9a8f444930791628f74dca20e
alexie0512 <alexie0512@163.com>
2021-04-19 19:52:15 +0800

    MyBatis自动生成数据库相关实体类的具体类

---
+++ src/main/java/com/testSpringMini/demo/dao/HogwartsTestUserMapper.java
@@ -0,0 +1,7 @@
+package com.testSpringMini.demo.dao;
+
+import com.testSpringMini.demo.common.MySqlExtensionMapper;
+import com.testSpringMini.demo.entity.HogwartsTestUser;
+
+public interface HogwartsTestUserMapper extends MySqlExtensionMapper<HogwartsTestUser> {
+}
